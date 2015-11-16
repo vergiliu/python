450c335852a9ce184805d663b35249c26564eeb0
vergiliu <vergiliu@gmail.com>
2015-11-16 20:18:20 +0100

    removed dry run

--- aws/start_ami_instance.py
+++ aws/start_ami_instance.py
@@ -4,7 +4,6 @@ import boto3
 import sys
 
 if __name__ == "__main__":
-    dry_run = True
     instances = None
     option = sys.argv[1] if len(sys.argv) > 1 else "nothing"
     ami_image = 'ami-60b6c60a'  # Amazon Linux
@@ -19,7 +18,7 @@ if __name__ == "__main__":
     if option == "start":
         # boto3 docs
         instances = ec2.create_instances(ImageId=ami_image, MinCount=1, MaxCount=1, InstanceType='t2.micro',
-                                         KeyName='temp_key', Monitoring={'Enabled': True}, DryRun=dry_run)
+                                         KeyName='temp_key', Monitoring={'Enabled': False})
         print(instances)
         for instance in instances:
             print(instance)
@@ -28,11 +27,6 @@ if __name__ == "__main__":
         # ids = ['instance-id-1', 'instance-id-2', ...]
         ec2.instances.filter(InstanceIds=instances).stop()
         ec2.instances.filter(InstanceIds=instances).terminate()
-
-        # instance = ec2.Instance('test-instance')
-        # response = instance.start(DryRun=dry_run)
-        # pprint(response)
-        # response = ec2_client.start_instances(DryRun=dry_run, InstanceIds=["required"])
 
     if option == "list":
         # Boto 3:  # Use the filter() method of the instances collection to retrieve
@@ -45,7 +39,7 @@ if __name__ == "__main__":
 
     if option == "keypair":
         key_name = 'temp_key'
-        keypair = ec2_client.create_key_pair(DryRun=dry_run, KeyName=key_name)
+        keypair = ec2_client.create_key_pair(KeyName=key_name)
         private_key = keypair['KeyMaterial']
         # save private key to a file
         with open('{}.pem'.format(key_name), "wt") as private_key_file:
