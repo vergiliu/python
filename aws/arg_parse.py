9c202b465cddb982e7e83e3cedf4b125c922efe3
vergiliu <vergiliu@gmail.com>
2016-08-30 21:56:47 +0200

    initial argparse w/ optional and mandatory list

--- aws/arg_parse.py
+++ aws/arg_parse.py
@@ -1,7 +1,10 @@
 import argparse as ap
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
-    parser.add_argument("test", help="this is a test")
+    # parser.add_argument("test", help="this is a test")
+    parser.add_argument("-v", help="run with debug logging", action="store_true")  # optional
+    parser.add_argument("-action", help="specify action to take on EC2 instance: create, stop, start, delete, list, list-running",
+                        default="list")
     args = parser.parse_args()
     # print(args.echo)
-    print("test={}".format(args.test))
+    print("all arguments={}".format(args))
