d7d94dd0faa24fbdf16e8cdb7188781d83019ae3
vergiliu <vergiliu@gmail.com>
2016-10-16 16:16:28 +0200

    lamdba hrank

---
+++ various/map_lambda.py
@@ -0,0 +1,12 @@
+def fib(i):ª
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 1
+    else:
+        return fib(i-2)+fib(i-1)
+
+if __name__ == "__main__":
+    N = int(input().strip())
+    pow3 = lambda X: X*X*X
+    print(list(map(pow3, [fib(i) for i in range(N)])))
