57ae71f71318105ebf9a6dbc7f5652ab5a6c69ee
vergiliu <vergiliu@gmail.com>
2017-05-08 22:24:57 +0200

    added folium sample, initial candlestick

--- udemy-py/folium_maps_1.py
+++ udemy-py/folium_maps_1.py
@@ -1,10 +1,14 @@
 import folium
 import pandas
+import random
 # create Map object
-m = folium.Map(location=[45.372, -121.696], zoom_start=8, tiles='Stamen Toner')
+m = folium.Map(location=[45.372, -121.696], zoom_start=8, tiles='Stamen Terrain')
+# tiles = 'Stamen Toner'
 
 my_data = pandas.read_csv("Volcanoes-USA.txt")
+random_color = ['red', 'green', 'blue', 'black']
 for lat, lon, v_name in zip(my_data['LAT'], my_data['LON'], my_data['NAME']):
-    folium.Marker(location=[lat, lon], popup=v_name).add_to(m)
+    colorful_icon = folium.Icon(icon='cloud', color=random_color[random.randrange(0, len(random_color)-1)])
+    folium.Marker(location=[lat, lon], popup=v_name, icon=colorful_icon).add_to(m)
 
 m.save(outfile="test.html")
