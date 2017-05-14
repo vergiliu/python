87056c685f324ef97309f5356b6d8f079416e06d
vergiliu <vergiliu@gmail.com>
2017-05-14 21:21:18 +0200

    updated date retrieval formatting

--- udemy-py/candlestick/stock_candlestick.py
+++ udemy-py/candlestick/stock_candlestick.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route('/<symbol_name>')
 def plot(symbol_name):
-    start = datetime.datetime(2015, 11, 1)
-    end = datetime.datetime(2016, 3, 10)
+    start = datetime.datetime(2016, 11, 1)
+    end = datetime.datetime(2017, 3, 10)
 
     if not symbol_name:
         symbol_name = "GOOG"
@@ -31,7 +31,7 @@ def plot(symbol_name):
     my_data["Height"] = abs(my_data.Close - my_data.Open)
 
     my_figure = figure(x_axis_type='datetime', width=1000, height=500, responsive=True,
-               title="price chart for {} [{} to {}]".format(symbol_name, start.date(), end.date()))
+                       title="Price chart for {} [{} to {}]".format(symbol_name, start.date(), end.date()))
     my_figure.grid.grid_line_alpha = 0.3
 
     hours_12 = 18 * 60 * 60 * 1000
@@ -39,10 +39,10 @@ def plot(symbol_name):
     my_figure.segment(my_data.index, my_data.High, my_data.index, my_data.Low, color="Black")
 
     my_figure.rect(my_data.index[my_data.Status == "Increase"], my_data.Middle[my_data.Status == "Increase"],
-           hours_12, my_data.Height[my_data.Status == "Increase"], fill_color="#CCFFFF", line_color="black")
+                   hours_12, my_data.Height[my_data.Status == "Increase"], fill_color="#CCFFFF", line_color="black")
 
     my_figure.rect(my_data.index[my_data.Status == "Decrease"], my_data.Middle[my_data.Status == "Decrease"],
-           hours_12, my_data.Height[my_data.Status == "Decrease"], fill_color="#FF3333", line_color="black")
+                   hours_12, my_data.Height[my_data.Status == "Decrease"], fill_color="#FF3333", line_color="black")
 
     script1, div1 = components(my_figure)
     cdn_js = CDN.js_files[0]
@@ -53,9 +53,11 @@ def plot(symbol_name):
                            cdn_css=cdn_css,
                            cdn_js=cdn_js)
 
+
 @app.route('/')
 def return_root():
     return "try going to /SYMBOL"
+
 
 # @app.route('/about/')
 # def about():
