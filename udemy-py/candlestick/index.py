f4365114e74b4dc42ce14bca8e414056340d5ba5
vergiliu <vergiliu@gmail.com>
2017-05-14 14:32:18 +0200

    flask + plot template

--- udemy-py/candlestick/index.py
+++ udemy-py/candlestick/index.py
@@ -1,25 +1,68 @@
-import datetime
-from pandas_datareader import data
-from bokeh.plotting import figure, show, output_file
+from bokeh.models import Title
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def plot():
+    from pandas_datareader import data
+    import datetime
+    from bokeh.plotting import figure, show, output_file
+    from bokeh.embed import components
+    from bokeh.resources import CDN
+
+    start = datetime.datetime(2015, 11, 1)
+    end = datetime.datetime(2016, 3, 10)
+
+    df = data.DataReader(name="GOOG", data_source="yahoo", start=start, end=end)
+
+    def inc_dec(c, o):
+        if c > o:
+            value = "Increase"
+        elif c < o:
+            value = "Decrease"
+        else:
+            value = "Equal"
+        return value
+
+    df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
+    df["Middle"] = (df.Open + df.Close) / 2
+    df["Height"] = abs(df.Close - df.Open)
+
+    p = figure(x_axis_type='datetime', width=1000, height=300, responsive=True)
+    # p.title = Title("Candlestick Chart")
+    p.grid.grid_line_alpha = 0.3
+
+    hours_12 = 12 * 60 * 60 * 1000
+
+    p.segment(df.index, df.High, df.index, df.Low, color="Black")
+
+    p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"],
+           hours_12, df.Height[df.Status == "Increase"], fill_color="#CCFFFF", line_color="black")
+
+    p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"],
+           hours_12, df.Height[df.Status == "Decrease"], fill_color="#FF3333", line_color="black")
+
+    script1, div1 = components(p)
+    cdn_js = CDN.js_files[0]
+    cdn_css = CDN.css_files[0]
+    return render_template("./plot.html",
+                           script1=script1,
+                           div1=div1,
+                           cdn_css=cdn_css,
+                           cdn_js=cdn_js)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
-    start_date = datetime.datetime(2017, 3, 1)
-    end_date = datetime.datetime(2017, 4, 10)
-    stock = "AAPL"
-    dr = data.DataReader(name=stock, data_source="yahoo", start=start_date, end=end_date)
-    print(dr)
-
-    my_graph = figure(x_axis_type='datetime', width=800, height=600, title=stock)  #, y_range=(130, 150))
-
-    twelve_hours = 12 * 60 * 60 * 1000
-    red = dr.Open > dr.Close
-    green = dr.Close > dr.Open
-
-    my_graph.segment(dr.index, dr.High, dr.index, dr.Low, color="blue")
-    my_graph.rect(dr.index[green], (dr.Open + dr.Close) / 2,
-                  twelve_hours, abs(dr.Open - dr.Close), color="green")
-    my_graph.rect(dr.index[red], (dr.Open + dr.Close) / 2,
-                  twelve_hours, abs(dr.Open - dr.Close), color="red")
-    output_file("data_stocks.html")
-    show(my_graph)
-    # online_data.index
+    app.run(debug=True)
