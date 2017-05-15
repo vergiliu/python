1cfd329eb470beb90f51dc81f6370c7cffd97700
vergiliu <vergiliu@gmail.com>
2017-05-15 22:02:06 +0200

    path can contain start date besides symbol

--- udemy-py/candlestick/stock_candlestick.py
+++ udemy-py/candlestick/stock_candlestick.py
@@ -1,21 +1,31 @@
+import datetime
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import CDN
 from flask import Flask, render_template
 from pandas_datareader import data
-import datetime
-from bokeh.plotting import figure, show, output_file
-from bokeh.embed import components
-from bokeh.resources import CDN
 
 app = Flask(__name__)
 
 
 @app.route('/<symbol_name>')
-def plot(symbol_name):
-    start = datetime.datetime(2016, 11, 1)
-    end = datetime.datetime(2017, 3, 10)
+@app.route('/<symbol_name>/<string:start_date>')
+def plot(symbol_name, start_date=None):
+    if start_date:
+        print("date={}".format(start_date))
+        # %Y  Year with century as a decimal number.
+        # %m  Month as a decimal number [01,12].
+        # %d  Day of the month as a decimal number [01,31].
+        start_moment = datetime.datetime.strptime(start_date, "%d%m%Y")
+    else:
+        print("no start moment given, last 10 days")
+        start_moment = datetime.date.today() - datetime.timedelta(days=10)
+    end_moment = datetime.datetime.today()
 
     if not symbol_name:
         symbol_name = "GOOG"
-    my_data = data.DataReader(name=symbol_name, data_source="google", start=start, end=end)
+    my_data = data.DataReader(name=symbol_name, data_source="yahoo", start=start_moment, end=end_moment)
 
     def inc_dec(c, o):
         if c > o:
@@ -31,7 +41,7 @@ def plot(symbol_name):
     my_data["Height"] = abs(my_data.Close - my_data.Open)
 
     my_figure = figure(x_axis_type='datetime', width=1000, height=500, responsive=True,
-                       title="Price chart for {} [{} to {}]".format(symbol_name, start.date(), end.date()))
+                       title="Price chart for {} [{} to {}]".format(symbol_name, start_moment, end_moment.date()))
     my_figure.grid.grid_line_alpha = 0.3
 
     hours_12 = 18 * 60 * 60 * 1000
@@ -56,7 +66,7 @@ def plot(symbol_name):
 
 @app.route('/')
 def return_root():
-    return "try going to /SYMBOL"
+    return "<html><body><span style='font-family: Verdana'>try going to <br>/SYMBOL or <br>/SYMBOL/03242017</span></body></html>"
 
 
 # @app.route('/about/')
@@ -65,4 +75,4 @@ def return_root():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, port=5555)
